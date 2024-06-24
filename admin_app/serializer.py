9a50407b08b6f715bdea660774807bb7b70a39f9
Rahul Raj <itsrahulraj.wst@gmail.com>
2024-06-24 13:06:00 +0530

    removed folder

--- admin_app/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CategoryInfoModel
-
-class DataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=CategoryInfoModel
-        fields=('catid','categoryname','handle','categoryhomeimage','categorysectionimage')
